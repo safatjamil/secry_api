75e995e5f42043531408f7e9deb9e304c57b5e8f
safatjamil <safaetxaamil@gmail.com>
2024-01-23 02:41:56 +0600

    refactoring

--- api/content_views.py
+++ api/content_views.py
@@ -4,14 +4,10 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from .serializers import *
-from .models import *
-from modules import auth
-from modules import cryptography
-from modules import query
-from modules import validations 
-from modules import jwt
-from modules import dbase_operations
+from .serializers import UserSerializer
+from .models import UserManager, CustomUser, Secret, EncKey
+from modules import auth, cryptography, query, \
+                    validations, jwt, dbase_operations
 
 
 # modules
@@ -23,7 +19,6 @@ query = query.Query()
 create = dbase_operations.Create()
 update = dbase_operations.Update()
 delete = dbase_operations.Delete()
-
 User = get_user_model()
 
 
@@ -40,19 +35,13 @@ def user_view_secrets(request):
             dict_["id"] = secret.id
             dict_["title"] = secret.title
             data.append(dict_)
-        
-        return Response({
-            "status": status.HTTP_200_OK,
-            "message": "Secrets",
-            "data": data,
-           })
-
+        return Response({"status": status.HTTP_200_OK,
+                         "message": "Secrets",
+                         "data": data})
     except:
-        return Response({
-            "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
-            "message": "Something went wrong",
-            "data" : {}
-           })
+        return Response({"status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                         "message": "Something went wrong",
+                         "data" : {}})
 
 
 @api_view(["POST"])
@@ -61,37 +50,28 @@ def create_secret(request):
     try:
         data = request.data
         user = request.user
-        if ("title" in data and data["title"].strip()!= "") and ("secret" in data and data["secret"].strip()!= ""):
+        if (("title" in data and data["title"].strip()!= "") and 
+            ("secret" in data and data["secret"].strip()!= "")):
             # encrypt the secret
             encrypted = encrypt.string(data["secret"].strip())
-            
             # save the secret
-            secret_data = {"user_id": user.id, "title": data["title"], "secret": encrypted["enc_string"]}
-            secret_id = create.secret(data = secret_data)
-            
+            secret_data = {"user_id": user.id, "title": data["title"], 
+                           "secret": encrypted["enc_string"]}
+            secret_id = create.secret(data=secret_data)
             # store encryption key in a differnt model
             key_data = {"secret_id": secret_id, "key": encrypted["key"]}
-            create.enckey(data = key_data)
-            
-            return Response({
-                "status": status.HTTP_201_CREATED,
-                "message": "Your secret has been created",
-                "data": data,
-               })
-
+            create.enckey(data=key_data)
+            return Response({"status": status.HTTP_201_CREATED,
+                             "message": "Your secret has been created",
+                             "data": data})
         else:
-            return Response({
-                "status": status.HTTP_400_BAD_REQUEST,
-                "message": "Please provide valid data",
-                "data": {}
-            })
-
+            return Response({"status": status.HTTP_400_BAD_REQUEST,
+                             "message": "Please provide valid data",
+                             "data": {}})
     except:
-        return Response({
-            "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
-            "message": "Something went wrong",
-            "data" : {}
-        })
+        return Response({"status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                         "message": "Something went wrong",
+                         "data" : {}})
 
 
 
@@ -106,28 +86,20 @@ def secret_details(request, secret_id):
         if secret["found"]:
             data["id"] = secret_id
             data["title"] = secret["content"]["title"]
-
             # decrypt the content
             encr_key = query.encr_key(secret_id)
             data["secret"] = decrypt.string(secret["content"]["secret"], encr_key["key"])
-            return Response({
-                "status": status.HTTP_200_OK,
-                "message": "Secret",
-                "data": data,
-               })
+            return Response({"status": status.HTTP_200_OK,
+                             "message": "Secret",
+                             "data": data})
         else:
-            return Response({
-                "status": status.HTTP_404_NOT_FOUND,
-                "message": "Your secret was not found",
-                "data" : {}
-               })
-
+            return Response({"status": status.HTTP_404_NOT_FOUND,
+                             "message": "Your secret was not found",
+                             "data" : {}})
     except:
-        return Response({
-            "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
-            "message": "Something went wrong",
-            "data" : {}
-           })
+        return Response({"status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                         "message": "Something went wrong",
+                         "data" : {}})
 
 
 @api_view(["PUT"])
@@ -136,39 +108,28 @@ def edit_secret(request, secret_id):
     try:
         data  = request.data
         user = request.user
-
         # check if the secret exists for this user
         secret = query.secret(secret_id, user.id)
         if secret["found"]:
             # encrypt the content
             encrypted = encrypt.string(data["new_secret"].strip())
             secret_data = {"title": data["title"], "secret": encrypted["enc_string"]}
-            
             # update the secret
-            update.secret(secret_id = secret_id, data = secret_data)
-            
+            update.secret(secret_id=secret_id, data=secret_data)
             # update the corresponding key
             key_data = {"key": encrypted["key"]}
-            update.enckey(secret_id = secret_id, data = key_data)
-
-            return Response({
-                "status": status.HTTP_201_CREATED,
-                "message": "Your secret has been updated",
-                "data": data,
-               })
-        
+            update.enckey(secret_id=secret_id, data=key_data)
+            return Response({"status": status.HTTP_201_CREATED,
+                             "message": "Your secret has been updated",
+                             "data": data})
         else:
-            return Response({
-                "status": status.HTTP_404_NOT_FOUND,
-                "message": "Your secret was not found",
-                "data" : {}
-               })
+            return Response({"status": status.HTTP_404_NOT_FOUND,
+                             "message": "Your secret was not found",
+                             "data" : {}})
     except:
-        return Response({
-            "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
-            "message": "Something went wrong",
-            "data" : {}
-            })
+        return Response({"status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                         "message": "Something went wrong",
+                         "data" : {}})
 
 
 @api_view(["DELETE"])
@@ -177,27 +138,19 @@ def delete_secret(request, secret_id):
     try:
         data  = request.data
         user = request.user
-
         # check if the secret exists for this user
         secret = query.secret(secret_id, user.id)
         if secret["found"]:
-            delete.secret(secret_id = secret_id)
-            delete.enckey(secret_id = secret_id)
-
-            return Response({
-                "status": status.HTTP_200_OK,
-                "message": "Your secret has been deleted",
-                "data": {},
-               })
+            delete.secret(secret_id=secret_id)
+            delete.enckey(secret_id=secret_id)
+            return Response({"status": status.HTTP_200_OK,
+                             "message": "Your secret has been deleted",
+                             "data": {}})
         else:
-            return Response({
-                "status": status.HTTP_404_NOT_FOUND,
-                "message": "Your secret was not found",
-                "data" : {}
-               })
+            return Response({"status": status.HTTP_404_NOT_FOUND,
+                             "message": "Your secret was not found",
+                             "data" : {}})
     except:
-        return Response({
-            "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
-            "message": "Something went wrong",
-            "data" : {}
-            })
+        return Response({"status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                         "message": "Something went wrong",
+                         "data" : {}})
