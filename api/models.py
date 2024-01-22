45589503fbc11dee2648bdec794b469752710443
safatjamil <safaetxaamil@gmail.com>
2024-01-23 02:44:14 +0600

    refactoring

--- api/models.py
+++ api/models.py
@@ -9,7 +9,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         extra_fields.setdefault("is_active", True)
-        user = self.model(email = self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -20,7 +20,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
-        user = self.model(email = self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -31,10 +31,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(max_length=40, unique=True)
     first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30, null = True, blank=True)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
-    is_superuser = models.BooleanField(default = False)
+    last_name = models.CharField(max_length=30, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     objects = UserManager()
 
     USERNAME_FIELD = "email"
